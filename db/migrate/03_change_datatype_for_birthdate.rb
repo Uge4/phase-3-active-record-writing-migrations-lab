3ad60ebf849fa27da5ad6ae6d550e083f7ff0ab9
eugene <eugeneholenstein@gmail.com>
2023-05-14 22:11:29 +1000

    fixed migration crud codealong

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
-    def change
+    def change up
         change_column :artists, :birthdate, :datatime
+    end
+
+    def change down
+        change_column :artists, :birthdate, :datetime
     end
   end
